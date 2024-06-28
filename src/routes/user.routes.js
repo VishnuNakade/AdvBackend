54a5096b056e04bc3f48d0f9f85dc8f832af21ab
Vishnu Nakade <vishnunakade@gmail.com>
2024-06-28 23:36:19 +0530

    fix logout functionality and remove console.log statement

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -42,12 +42,12 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refereshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
 router.route("/current-user").get(verifyJWT, getCurrentUser)
+
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
-
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
-
-router.route("cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+router.route("cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
 router.route("/history").get(verifyJWT,getWatchHistory)
